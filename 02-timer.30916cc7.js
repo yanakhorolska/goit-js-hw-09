f2b8d6e4286893f775708535dbc9fdeaf8279011
yanakhorolska <92363882+yanakhorolska@users.noreply.github.com>
2022-09-20 15:00:56 +0000

    Deploying to gh-pages from @ yanakhorolska/goit-js-hw-09@2616b155ac68afbde5b12e0b16a3621e78df6d63 🚀

---
+++ 02-timer.30916cc7.js
@@ -0,0 +1,2 @@
+({startBtn:document.querySelector("[data-start]"),daysCounter:document.querySelector("[data-start]"),hoursCounter:document.querySelector("[data-start]"),minutesCounter:document.querySelector("[data-start]"),secondsCounter:document.querySelector("[data-start]")}).startBtn.addEventListener("click",onStartTimer);
+//# sourceMappingURL=02-timer.30916cc7.js.map
---
+++ 02-timer.33f9c7e9.js
@@ -0,0 +1,2 @@
+[document.querySelector("[data-start]"),document.querySelector("[data-start]"),document.querySelector("[data-start]"),document.querySelector("[data-start]"),document.querySelector("[data-start]")][0].addEventListener("click",onStartTimer);
+//# sourceMappingURL=02-timer.33f9c7e9.js.map
