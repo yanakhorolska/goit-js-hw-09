2cc830ed1acecfc165a72e67f42ef4f94619278a
yanakhorolska <92363882+yanakhorolska@users.noreply.github.com>
2022-09-20 18:36:39 +0000

    Deploying to gh-pages from @ yanakhorolska/goit-js-hw-09@aa6dacf5011034257d3f8fb514754823357a46b7 🚀

--- 02-timer.33f9c7e9.js
+++
@@ -1,2 +0,0 @@
-[document.querySelector("[data-start]"),document.querySelector("[data-start]"),document.querySelector("[data-start]"),document.querySelector("[data-start]"),document.querySelector("[data-start]")][0].addEventListener("click",onStartTimer);
-//# sourceMappingURL=02-timer.33f9c7e9.js.map
