2616b155ac68afbde5b12e0b16a3621e78df6d63
yanakhorolska <yana.khorolska@protonmail.com>
2022-09-20 17:00:23 +0200

    update styles for timer

---
+++ src/js/02-timer.js
@@ -0,0 +1,11 @@
+const refs = {
+    startBtn: document.querySelector('[data-start]'),
+    daysCounter: document.querySelector('[data-start]'),
+    hoursCounter: document.querySelector('[data-start]'),
+    minutesCounter: document.querySelector('[data-start]'),
+    secondsCounter: document.querySelector('[data-start]'),
+};
+
+
+refs.startBtn.addEventListener('click', onStartTimer);
+
